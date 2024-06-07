9b1ddd143192975fdaae07708841e614c18ca68f
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2024-06-06 21:09:55 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -13,3 +13,22 @@ r = criarvar(4)
 
 for nome_var in r:
     print(nome_var)
+
+
+
+# CRIAÇÃO DE VARIÁVEIS E DESEMPATANDO DA LISTA 
+# CRIANDO NOVOS DICIONÁRIOS
+
+
+
+
+a,b,c,d =  [1,3,6,6,]
+d = dict(a=a,b=b,c=c,d=d)
+print(d)
+
+a,b,c,d =  [1,3,6,6,]
+d = dict(a=a,b=b,c=c,d=d)
+a = list(d.keys())
+f =  tuple(d.values())
+g =  d.items()
+print(a, f)
